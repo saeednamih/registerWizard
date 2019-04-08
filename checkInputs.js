79ec04c1bfd4f9a5967054a4bc2a81b3beec8004
unknown <saeed>
2019-04-08 19:55:51 +0300

    starting

---
+++ checkInputs.js
@@ -0,0 +1,40 @@
+//check if the textInput IsNoEmpty the function get a string parameter
+//retrun * requird if the parameter textInput IsEmpty
+// else return true
+
+function IsNoEmpty(textInput){
+   
+    return true;
+};
+
+//check the name length the function get a string parameter
+// return * Minimum 3 characters 
+//else return true
+function checkInputLength(textInput){
+    return true;
+};
+
+//check the email  the function get a string parameter
+// return * Insert a valid Email address, valid mail = aaa@aaa.aaa
+//else return true
+function checkEmail(textInput){
+    return true;
+};
+
+//check if the Number grther than 0 , the function get a number as a parameter
+//return true iff number>0
+//else return * Number must be positive
+
+function checkNumber(textInput){
+    return true;
+};
+
+//check the URL  the function get a string parameter
+// return * Insert a valid URL address, valid URL = stret with www. or https://www. or https://
+//else return true
+function checkURL(textInput){
+    return true;
+}
+
+
+
